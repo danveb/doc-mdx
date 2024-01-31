cd62693693fbc04276d67e9608ccb33613f293a9
Daniel Bae <danibae91@icloud.com>
2024-01-30 22:51:07 -0600

    react routing enabled, work on notFound component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
+import NotFound from "./components/NotFound.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css"
+
+// createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
