77e513c1dcfd428a92b295b73cdc6696daf550d6
Daniel Bae <danibae91@icloud.com>
2024-02-01 16:13:15 -0600

    implement dark mode toggle with context api; only present in code snippet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState, createContext } from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Post from "./components/Post";
 
+// createContext
+export const ThemeContext = createContext();
+
 export default function App() {
+  // useState 
+  const [darkMode, setDarkMode] = useState(true);
+
+  // value to be darkMode, setDarkMode
+  const value = {
+    darkMode, setDarkMode
+  }
+
   return (
-    <>
+    <ThemeContext.Provider value={value}>
       <Navbar />
       <About />
       <Post />
-    </>
+    </ThemeContext.Provider>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,24 @@
-import { LogoIcon, MoonIcon } from "./assets/icons";
+import { LogoIcon, MoonIcon, SunIcon } from "./assets/icons";
 import "../styles/Navbar.css";
+import { useContext } from "react";
+import { ThemeContext } from "../App";
 
 export default function Navbar() {
+  // useContext 
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+
   return (
     <header className="navbar">
       <div className="nav__wrapper container">
         <div className="nav__logo">
           <LogoIcon />
           <p>doc-mdx</p>
-          <button className="nav__icon"><MoonIcon /></button>
+          <button
+            onClick={() => setDarkMode(!darkMode)}
+            className="nav__icon"
+          >
+            {darkMode ? <MoonIcon /> : <SunIcon />}
+          </button>
         </div>
       </div>
     </header>
